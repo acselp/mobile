b2778992c9ab89f10e8ee7287c4fea42714217ee
Verj <virgiliu.plesca@iis.utm.md>
2022-11-27 16:43:15 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import AppStack from './src/navigation/AppStack';
+import AuthStack from './src/navigation/AuthStack';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    
+    <NavigationContainer>
+      {/* <AppStack />     */}
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
+
+export default App;
